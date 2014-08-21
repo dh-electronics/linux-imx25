7da6b63b727b93fa0de50a532aec24e61eb25083
Ludwig Zenz <lzenz@dh-electronics.de>
2014-08-21 15:07:30 +0200

    add support for ebox baseboard

--- drivers/net/ethernet/davicom/dm9000.c
+++ drivers/net/ethernet/davicom/dm9000.c
@@ -1436,7 +1436,7 @@ dm9000_probe(struct platform_device *pdev)
 		goto out;
 	}
 
-	db->io_addr = ioremap(db->addr_res->start, iosize);
+	db->io_addr = ioremap_nocache(db->addr_res->start, iosize);
 
 	if (db->io_addr == NULL) {
 		dev_err(db->dev, "failed to ioremap address reg\n");
@@ -1454,7 +1454,7 @@ dm9000_probe(struct platform_device *pdev)
 		goto out;
 	}
 
-	db->io_data = ioremap(db->data_res->start, iosize);
+	db->io_data = ioremap_nocache(db->data_res->start, iosize);
 
 	if (db->io_data == NULL) {
 		dev_err(db->dev, "failed to ioremap data reg\n");
